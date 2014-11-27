19f1912e3693f094e5d6321c1689aa5690afb7d9
Przemek Lekston <p.lekston@gmail.com>
2014-11-27 15:04:17 +0100

    Increase APM RC Output freq on PX4

--- libraries/AP_HAL_PX4/RCOutput.cpp
+++ libraries/AP_HAL_PX4/RCOutput.cpp
@@ -447,9 +447,8 @@ void PX4RCOutput::_send_outputs(void)
         goto update_pwm;
     }
 
-    // always send at least at 20Hz, otherwise the IO board may think
-    // we are dead
-    if (now - _last_output > 50000) {
+    // always send at least at 50Hz to minimize jitter
+    if (now - _last_output > 19500) {
         _need_update = true;
     }
 
