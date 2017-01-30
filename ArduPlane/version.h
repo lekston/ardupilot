ed980f2e514e590f848941110c476aee5a0510e4
Przemek Lekston <plekston@flytechuav.com>
2017-01-31 00:12:57 +0100

    [Plane] Mark version with FT2

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.7.1FT"
+#define THISFIRMWARE "ArduPlane V3.7.1FT2"
 #define FIRMWARE_VERSION 3,7,1,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #ifndef GIT_VERSION
