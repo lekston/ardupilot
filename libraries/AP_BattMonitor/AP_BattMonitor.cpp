c32b31708cd4dbe96d6dcf21be5683995d2dc500
Przemek Lekston <plekston@flytechuav.com>
2019-05-12 17:48:01 +0200

    [BattMon] Allow 3 instances.

--- libraries/AP_BattMonitor/AP_BattMonitor.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor.cpp
@@ -25,6 +25,12 @@ const AP_Param::GroupInfo AP_BattMonitor::var_info[] = {
     // @Path: AP_BattMonitor_Params.cpp
     AP_SUBGROUPINFO(_params[1], "2_", 24, AP_BattMonitor, AP_BattMonitor_Params),
 
+#if AP_BATT_MONITOR_MAX_INSTANCES > 2
+    // @Group: 3_
+    // @Path: SRV_Channel.cpp
+    AP_SUBGROUPINFO(_params[2], "3_",  25, AP_BattMonitor, AP_BattMonitor_Params),
+#endif
+
     AP_GROUPEND
 };
 
